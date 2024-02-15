e3419b6dd12e325c996ba44e9f24db79a4d927af
Fly-Away <soundgraphia@gmail.com>
2024-02-15 09:09:39 +0700

    initial commit

---
+++ src/main/java/com/bhakti/bktapijava2/BktapiJava2Application.java
@@ -0,0 +1,13 @@
+package com.bhakti.bktapijava2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BktapiJava2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BktapiJava2Application.class, args);
+	}
+
+}
