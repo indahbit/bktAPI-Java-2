235359bd50a617b88f1741d7d255838f4de19936
Fly-Away <soundgraphia@gmail.com>
2024-03-08 08:56:58 +0700

    update agar cron value bisa di config dari env variable

--- src/main/java/com/bhakti/bktapijava2/scheduler/MergePDFScheduler.java
+++ src/main/java/com/bhakti/bktapijava2/scheduler/MergePDFScheduler.java
@@ -3,6 +3,7 @@ package com.bhakti.bktapijava2.scheduler;
 import com.bhakti.bktapijava2.service.ITandaTerimaService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -10,11 +11,15 @@ import org.springframework.stereotype.Component;
 @Component
 public class MergePDFScheduler {
 
+    @Value("${mergepdf.cron}")
+    private String mergepdfCronParameter;
+
     @Autowired
     private ITandaTerimaService tandaTerimaService;
 
-    @Scheduled(cron = "0 59 23 * * ?")
+    @Scheduled(cron = "${mergepdf.cron}")
     public void mergePdfFKSJFPJob() {
+        log.info("Cron config value : " + mergepdfCronParameter);
         tandaTerimaService.mergePdfByNoKwitansi();
 
         log.info("Job merge selesai dijalankan.");
